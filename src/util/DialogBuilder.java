86a3802d485d5d7e0dbcb7da53eb65e077dd9c6c
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-05-09 21:52:02 +0100

    Update DialogBuilder.java

--- src/util/DialogBuilder.java
+++ src/util/DialogBuilder.java
@@ -1,13 +1,19 @@
 package util;
 
 import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.util.Pair;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.GridPane;
 
 public class DialogBuilder {
 	private DialogBuilder() {}
@@ -31,6 +37,67 @@ public class DialogBuilder {
 			}
 		});
 		
+		return dialog;
+	}
+	
+	public static Dialog<Pair<String, String>> getTwoTextFieldInputDialog(String title, String headerText, String firstFieldName, 
+			String secondFieldName, boolean isOptionalSecondField) {
+		Dialog<Pair<String, String>> dialog = new Dialog<>();
+
+		dialog.setTitle(title);
+		dialog.setHeaderText(headerText);
+
+		dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(20, 150, 10, 10));
+
+		TextField firstField = new TextField();
+		firstField.setPromptText(firstFieldName);
+
+		TextField secondField = new TextField();
+		secondField.setPromptText(secondFieldName);
+
+		grid.add(new Label(firstFieldName), 0, 0);
+		grid.add(firstField, 1, 0);
+
+		grid.add(new Label(secondFieldName), 0, 1);
+		grid.add(secondField, 1, 1);
+
+		Button okButton = (Button) dialog.getDialogPane().lookupButton(ButtonType.OK);
+
+		okButton.setDisable(true);
+
+		firstField.textProperty().addListener((observable, oldValue, newValue) -> {
+			boolean areRequiredFieldsEmpty = newValue.trim().isEmpty();
+
+			if (!isOptionalSecondField) {
+				areRequiredFieldsEmpty = areRequiredFieldsEmpty & secondField.getText().trim().isEmpty();
+			}
+
+			okButton.setDisable(areRequiredFieldsEmpty);
+		});
+
+		if (!isOptionalSecondField) {
+			secondField.textProperty().addListener((observable, oldValue, newValue) -> {
+				boolean areRequiredFieldsEmpty = newValue.trim().isEmpty() & firstField.getText().trim().isEmpty();
+
+				okButton.setDisable(areRequiredFieldsEmpty);
+			});
+		}
+
+		dialog.getDialogPane().setContent(grid);
+
+		dialog.setResultConverter(dialogButton -> {
+			if (dialogButton == ButtonType.OK) {
+				return new Pair<>(firstField.getText(), secondField.getText());
+			}
+
+			return null;
+		});
+
 		return dialog;
 	}
 	
