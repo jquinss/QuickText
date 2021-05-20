003cef0fb048d00335ce86a0ec95e0e762211481
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-05-20 10:59:19 +0100

    Update FileTreeItem.java

--- src/control/FileTreeItem.java
+++ src/control/FileTreeItem.java
@@ -3,8 +3,13 @@ package control;
 import data.FileItem;
 import javafx.scene.control.ContextMenu;
 import javafx.scene.control.TreeItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FileTreeItem extends TreeItem<FileItem> {
+	private static final String FOLDER_IMG = "/images/folder_img.png";
+	private static final String TXT_IMG = "/images/txt_img.png";
+	private static final String HTML_IMG = "/images/html_img.png";
 	private ContextMenu contextMenu;
 	
 	public FileTreeItem(FileItem fileItem) {
@@ -17,5 +22,17 @@ public class FileTreeItem extends TreeItem<FileItem> {
 	
 	public ContextMenu getContextMenu() {
 		return contextMenu;
+	}
+	
+	public String getImgURL() {
+		if (getValue().isDirectory()) {
+			return FOLDER_IMG;
+		}
+		else if (getValue().isPlainTextTemplate()) {
+			return TXT_IMG;
+		}
+		else {
+			return HTML_IMG;
+		}
 	}
 }
