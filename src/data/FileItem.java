c8ca45ed26a2b76a163d7a2deb3c59e9dece06cd
Joaquin <47039871+jquinss@users.noreply.github.com>
2021-06-08 19:45:46 +0100

    Update FileItem.java

--- src/data/FileItem.java
+++ src/data/FileItem.java
@@ -2,79 +2,23 @@ package data;
 
 import java.io.File;
 
-public class FileItem {
-	private File file;
-	private String description;
-	private static final String PLAIN_TEXT_EXT = ".txt";
-	private static final String HTML_EXT = ".html";
-	private boolean isFile = false;
-	private boolean isDirectory = false;
-	private boolean isRootDirectory = false;
-	private boolean isPlainTextTemplate = false;
-	private boolean isHTMLTemplate = false;
+public abstract class FileItem {
+	File file;
+	String description = "";
 	
 	public FileItem(File file) {
 		this.file = file;
-		setFileType();
 	}
 	
 	public void setDescription(String description) {
 		this.description = description;
 	}
 	
-	public File getFile() {
-		return file;
-	}
-	
-	public void setIsRoot(boolean isRootDirectory) {
-		this.isRootDirectory = isRootDirectory;
-	}
-	
 	public String getDescription() {
 		return description;
 	}
 	
-	private void setFileType() {
-		if (file.isFile()) {
-			isFile = true;
-			setFileSubType();
-		}
-		else if (file.isDirectory()) {
-			isDirectory = true;
-		}
-	}
-	
-	public boolean isFile() {
-		return isFile;
-	}
-	
-	public boolean isDirectory() {
-		return isDirectory;
-	}
-	
-	public boolean isRootDirectory() {
-		return isRootDirectory;
-	}
-	
-	private void setFileSubType() {
-		String fileName = file.toString();
-		int txtExtLength = PLAIN_TEXT_EXT.length();
-		int htmlExtLength = HTML_EXT.length();
-		int fileLength = fileName.length();
-		
-		if (fileName.regionMatches(true, fileLength - txtExtLength, PLAIN_TEXT_EXT, 0, txtExtLength)) {
-			isPlainTextTemplate = true;
-		}
-		else if (fileName.regionMatches(true, fileLength - htmlExtLength, HTML_EXT, 0, htmlExtLength)) {
-			isHTMLTemplate = true;
-		}
-	}
-	
-	public boolean isPlainTextTemplate() {
-		return isPlainTextTemplate;
-	}
-	
-	public boolean isHTMLTemplate() {
-		return isHTMLTemplate;
+	public File getFile() {
+		return file;
 	}
 }
