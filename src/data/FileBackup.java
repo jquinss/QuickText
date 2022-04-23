8b99d114faf3a36019024e3bb0f7ce2e8ce93410
Joaquin <47039871+jquinss@users.noreply.github.com>
2022-04-23 22:40:45 +0100

    Created FileBackup class

---
+++ src/data/FileBackup.java
@@ -0,0 +1,44 @@
+package data;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.FileTime;
+import java.security.NoSuchAlgorithmException;
+
+import util.HashCalculator;
+
+public class FileBackup {
+	private File file;
+	private String fileSHA256;
+	
+	public FileBackup(File file) throws FileNotFoundException, IOException {
+		this.file = file;
+		fileSHA256 = getFileSHA256();
+	}
+	
+	public File getFile() {
+		return file;
+	}
+	
+	public String getOrigFileSHA256() {
+		return fileSHA256;
+	}
+	
+	public String getFileSHA256() throws FileNotFoundException, IOException {
+		String fileSHA256 = null;
+		try {
+			fileSHA256 = HashCalculator.calculateFileHash("SHA-256", file.toString());
+		}
+		catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		
+		return fileSHA256;
+	}
+	
+	public FileTime getCreationTime() throws IOException {
+		return (FileTime) Files.getAttribute(file.toPath(), "basic:creationTime");
+	}
+}
