94763f557e4caaa742f38243a636ab8262c359ea
Joaquin <47039871+jquinss@users.noreply.github.com>
2022-04-24 20:35:57 +0100

    created createBackup() method

--- src/controllers/BackupsPaneController.java
+++ src/controllers/BackupsPaneController.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
@@ -7,13 +9,25 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import data.FileBackup;
+import util.ZipUtil;
+import managers.SettingsManager;
+
 public class BackupsPaneController {
 
     @FXML
-    private TableView<?> backupsTableView;
+    private TableView<FileBackup> backupsTableView;
 
     @FXML
-    private TableView<?> scheduledBackupsTableView;
+    private TableView<?> scheduledBackupTasksTableView;
 
     @FXML
     private DatePicker scheduledDateTime;
@@ -27,30 +41,70 @@ public class BackupsPaneController {
     private QuickTextController quickTextController;
     
     private Stage stage;
+    
+    private ObservableList<FileBackup> fileBackupObsList = FXCollections.observableArrayList();
 
     @FXML
     void createBackup(ActionEvent event) {
-
+    	Path source = Paths.get(SettingsManager.getInstance().getAppDir());
+    	String destZipFile = Paths.get(SettingsManager.getInstance().getBackupsDir(), generateBackupName()).toString();
+    	String[] ignoredFiles = new String[] {SettingsManager.getInstance().getBackupsDir(), SettingsManager.getInstance().getSettingsPath()};
+    	
+    	try {
+			ZipUtil.zipFolder(source, destZipFile, ignoredFiles);
+			FileBackup fileBackup = new FileBackup(new File(destZipFile));
+			fileBackupObsList.add(fileBackup);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
-    void createScheduledBackup(ActionEvent event) {
-
+    void createScheduledBackupTask(ActionEvent event) {
+    	System.out.println("Creating a backup task");
+    	/* TO DO */
     }
 
     @FXML
     void deleteBackup(ActionEvent event) {
-
+    	System.out.println("Deleting a backup file");
+    	/* TO DO */
     }
 
     @FXML
-    void deleteScheduledBackup(ActionEvent event) {
-
+    void deleteScheduledBackupTask(ActionEvent event) {
+    	System.out.println("Deleting a backup task");
+    	/* TO DO */
     }
 
     @FXML
     void loadBackup(ActionEvent event) {
-
+    	System.out.println("Loading backup");
+    	/* TO DO */
+    }
+    
+    private void loadBackupFiles() {
+    	System.out.println("Loading backup files");
+    	/* TO DO */
+    }
+    
+	private String generateBackupName() {
+		StringBuilder name = new StringBuilder();
+		name.append("backup-");
+		name.append(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMdd.HHmmss")));
+		name.append(".zip");
+		return name.toString();
+	}
+    
+    @FXML
+    public void initialize() {
+    	try {
+			Files.createDirectory(new File(SettingsManager.getInstance().getBackupsDir()).toPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    	backupsTableView.setItems(fileBackupObsList);
+    	loadBackupFiles();
     }
 
     void setQuickTextController(QuickTextController quickTextController) {
